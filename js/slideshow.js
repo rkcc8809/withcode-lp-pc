ac5c41ce1d339d7daf41e326a03ec64904387976
riku.sato <rkcc8809@gmail.com>
2024-08-31 12:19:24 +0900

    16th-commit

--- js/slideshow.js
+++ js/slideshow.js
@@ -28,6 +28,17 @@ $('.slider').slick({
   ]
 });
 
+$(function(){
+    const tgtSelectors = ['.begin-wrap__con__box__above', '.begin-wrap__con__box__under', '.act-wrap__con__box__above', '.act-wrap__con__box__under'];
+    
+    setTimeout(function(){
+        tgtSelectors.forEach(function(tgtSelector){
+            $(tgtSelector).matchHeight();
+        });
+    }, 750);  // 750ミリ秒遅延させて高さ調整を実行
+});
+
+
 $('.case-wrap__con__scroll__list').slick({
   autoplay: true, //自動スクロール
     autoplaySpeed: 0, //自動再生時のスライド切り替えの時間
